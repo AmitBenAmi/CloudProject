06e98805dc7643469b2cf31977bae2687a73af23
DanielleZror <daniellezror@gmail.com>
2018-01-19 13:37:18 +0200

    itam

---
+++ MicroServices/Items/src/main/java/com/cloudproject/items/models/Item.java
@@ -0,0 +1,9 @@
+package com.cloudproject.items.models;
+
+public class Item {
+	private String ID;	
+	private String name;
+	private String price;
+	private String currency;
+	private String image;
+}
