98126b72d3c477c81e218d0b0f79100618454d95
AmitBenAmi <benamiamit0@gmail.com>
2018-01-31 13:04:53 +0200

    feat(Bluemix): Added environment variables insted of const values for the URLs

--- Application/src/main/java/server/WebServer.java
+++ Application/src/main/java/server/WebServer.java
@@ -1,14 +1,18 @@
 package server;
+
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
 import spark.Spark;
+import org.apache.commons.lang3.ObjectUtils;
 
 public class WebServer {
 
 	public static void main(String[] args) throws IllegalArgumentException, UnsupportedEncodingException {
-		Spark.port(8080);
+		Integer applicationPort = getNumericEnvVariable("portNumber");
+		
+		Spark.port(applicationPort != null ? applicationPort : 8080);
 
 		Spark.staticFiles.location("/shop");
 
@@ -17,13 +21,37 @@ public class WebServer {
 		// Redirect to services
 		Map<String, String> servicesRouting = new HashMap<String, String>() {
 			{
-				put("/api/items", "http://localhost:8083");
-				put("/api/cart", "http://localhost:8082");
-				put("/api/orders", "http://localhost:8081");
-				put("/api/users", "http://localhost:8084");
+				put("/api/items", String.format("%s:%d", ObjectUtils.firstNonNull(getStringEnvVariable("itemsUrl"), "http://localhost"), ObjectUtils.firstNonNull(getNumericEnvVariable("itemsPort"), 8083)));
+				put("/api/cart", String.format("%s:%d", ObjectUtils.firstNonNull(getStringEnvVariable("cartUrl"), "http://localhost"), ObjectUtils.firstNonNull(getNumericEnvVariable("cartPort"), 8082)));
+				put("/api/orders", String.format("%s:%d", ObjectUtils.firstNonNull(getStringEnvVariable("ordersUrl"), "http://localhost"), ObjectUtils.firstNonNull(getNumericEnvVariable("ordersPort"), 8081)));
+				put("/api/users", String.format("%s:%d", ObjectUtils.firstNonNull(getStringEnvVariable("usersUrl"), "http://localhost"), ObjectUtils.firstNonNull(getNumericEnvVariable("usersPort"), 8084)));
 			}
 		};
 
 		Spark.before(new MicroServicesFilter(servicesRouting));
+	}
+	
+	private static Integer getNumericEnvVariable(String envVarName) {
+		Integer envVarValue = null;
+		try {
+			envVarValue = Integer.parseInt(System.getenv(envVarName));
+		}
+		catch (Exception e) {
+			System.out.println(String.format("%s environment variable isn't defined", envVarName));
+		}
+		
+		return envVarValue;
+	}
+	
+	private static String getStringEnvVariable(String envVarName) {
+		String envVarValue = null;
+		try {
+			envVarValue = System.getenv(envVarName).toString();
+		}
+		catch (Exception e) {
+			System.out.println(String.format("%s environment variable isn't defined", envVarName));
+		}
+		
+		return envVarValue;
 	}
 }
