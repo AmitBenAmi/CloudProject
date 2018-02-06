9168b285b30114828b9d200df3dc3f1c07220723
AmitBenAmi <benamiamit0@gmail.com>
2018-02-06 23:33:12 +0200

    Showing the items in the cart

--- Application/src/main/resources/shop/js/cart.js
+++ Application/src/main/resources/shop/js/cart.js
@@ -8,11 +8,63 @@ function getCartItemsOfUser() {
         xhrFields: {
             withCredentials: true
         }
-    }).done(function(items) {
-        console.log(items);
+    }).done(function(cartItems) {
+        //$('#cart-products__items-count').text(items.length);
+
+        if (cartItems.length > 0) {
+            getCartItemsDetails(cartItems);
+        }
     }).fail(function(result) {
         alert('error loading items in the cart');
     })
+}
+
+function getCartItemsDetails(cartItems) {
+    let ids = [];
+    for (let i = 0; i < cartItems.length; i++) {
+        let cartItem = cartItems[i];
+        let itemId = cartItem._id.split(':')[1];
+        ids.push(itemId);
+    }
+
+    getItemsByIds(ids, function (items) {
+        populateCartItems(items, cartItems);
+    });
+}
+
+function populateCartItems(items, cartItems) {
+    let cartItemsList = $('#cart-items');
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        let template = getClonedCartItemTemplate();
+
+        let itemQuantity = getItemQuantity(item, cartItems);
+
+        template.find('img.ref-img').attr('src', `data:image/jpeg;base64,${item.image}`);
+        template.find('td.cart-item__name').text(item.name);
+        template.find('input.cart-item__quantity').attr('placeholder', itemQuantity);
+        template.find('td.cart-item__price').text(`${item.price}${item.currency}`);
+        template.find('td.cart-item__total').text(`${item.price * itemQuantity}${item.currency}`);
+        cartItemsList.append(template);
+        template.show();
+    }
+}
+
+function getItemQuantity(item, cartItems) {
+    for (let i = 0; i < cartItems.length; i++) {
+        let cartItem = cartItems[i];
+        let cartItemId = cartItem._id.split(':')[1];
+
+        if (cartItemId === item._id) {
+            return cartItem.quantity
+        }
+    }
+
+    return 0;
+}
+
+function getClonedCartItemTemplate() {
+    return $("#cart-item-template").clone();
 }
 
 $(document).ready(function() {
