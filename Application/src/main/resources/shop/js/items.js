80a5db49172936c95a7bcc76ab261d0816ede8e8
AmitBenAmi <benamiamit0@gmail.com>
2018-02-08 10:01:45 +0200

    Trying to fix Cross Domain

--- Application/src/main/resources/shop/js/items.js
+++ Application/src/main/resources/shop/js/items.js
@@ -115,6 +115,7 @@ function addToCartFunction(itemId) {
                     'username': username,
                     'quantity' : 1
                 }),
+                crossDomain: true,
                 xhrFields: {
                     withCredentials: true
                 },
