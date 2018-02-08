039a4779e60ebec321662fb3f096a830fb9336f9
AmitBenAmi <benamiamit0@gmail.com>
2018-02-08 10:45:31 +0200

    fix

--- Application/src/main/resources/shop/js/login.js
+++ Application/src/main/resources/shop/js/login.js
@@ -76,7 +76,7 @@ function setCookie(name,value,days) {
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = `${name}=${value || ""}${expires}; path=/; Domain=${document.domain === 'localhost' ? 'localhost' : 'eu-de.mybluemix.net'}`;
+    document.cookie = `${name}=${value || ""}${expires}; path=/; Domain=${document.domain === 'localhost' ? 'localhost' : '.eu-de.mybluemix.net'}`;
 }
 
 function eraseCookie(name) {   
